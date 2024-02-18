8c789302cb3be8f7930707fdc11d7cebfc66dd74
michaltu <michal.tusz12@gmail.com>
2024-02-18 23:29:34 +0100

    added jpa to pom

--- src/main/java/com/tradingApp/tradingApp/model/UserEntity.java
+++ src/main/java/com/tradingApp/tradingApp/model/UserEntity.java
@@ -1,4 +1,16 @@
 package com.tradingApp.tradingApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.context.annotation.Primary;
+
+@Entity
 public class UserEntity {
+
+    @Id()
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
 }
