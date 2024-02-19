a157efbf43f85deee003fb627ae1eae5dad021a8
michaltu <michal.tusz12@gmail.com>
2024-02-19 10:47:29 +0100

    Introduced basics of userEntity model

--- src/main/java/com/tradingApp/tradingApp/model/UserEntity.java
+++ src/main/java/com/tradingApp/tradingApp/model/UserEntity.java
@@ -1,15 +1,39 @@
 package com.tradingApp.tradingApp.model;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Date;
+import java.util.List;
+
 @Entity
 public class UserEntity {
 
     @Id()
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private double balance;
+    @Nullable
+    private String name;
+    @Nullable
+    private String surename;
+    private Date createdDate;
+    private double currentFeeFlat;
+    private double currentFeePercentage;
+    private int level;
 
+    //ratings
+    //transactions
+    //topUps
+    //previouslyUsedPasswords;
+    //profilePic
+    //reports
+    //roles
 }
