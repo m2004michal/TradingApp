6c8faa9a11a2fc53544375344151e5319aec82d2
m2004michal <michal.tusz12@gmail.com>
2024-02-21 13:53:01 +0100

    created category model

--- src/main/java/com/tradingApp/tradingApp/model/Enums/Game.java
+++ src/main/java/com/tradingApp/tradingApp/model/Enums/Game.java
@@ -1,9 +1,6 @@
 package com.tradingApp.tradingApp.model.Enums;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +18,7 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @OneToMany
     private List<Category> categories;
 
 
--- src/main/java/com/tradingApp/tradingApp/model/Enums/Listing.java
+++ src/main/java/com/tradingApp/tradingApp/model/Enums/Listing.java
@@ -1,23 +1,21 @@
 package com.tradingApp.tradingApp.model.Enums;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Category {
+public class Listing {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    String name;
-    @OneToMany
-    List<Listing> listings;
 }
