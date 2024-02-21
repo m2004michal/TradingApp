2b09977130884aad7ec6501130446860fcdcc690
m2004michal <michal.tusz12@gmail.com>
2024-02-21 19:58:35 +0100

    added lombok @ to item class

--- src/main/java/com/tradingApp/tradingApp/model/Item.java
+++ src/main/java/com/tradingApp/tradingApp/model/Item.java
@@ -5,8 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
