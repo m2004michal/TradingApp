92650a1d425f3ef8fbc32e0bf994b575555c0838
m2004michal <michal.tusz12@gmail.com>
2024-02-22 13:05:59 +0100

    added VerificationToken model class

---
+++ src/main/java/com/tradingApp/tradingApp/model/VerificationToken.java
@@ -0,0 +1,23 @@
+package com.tradingApp.tradingApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = FetchType.LAZY)
+    private UserEntity user;
+    private Instant expiryDate;
+}
