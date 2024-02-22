3753459bb10f5d1143c169f7255c94512a534517
m2004michal <michal.tusz12@gmail.com>
2024-02-22 13:08:34 +0100

    created RefreshTokenRepository

---
+++ src/main/java/com/tradingApp/tradingApp/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.tradingApp.tradingApp.repository;
+
+import com.tradingApp.tradingApp.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+}
