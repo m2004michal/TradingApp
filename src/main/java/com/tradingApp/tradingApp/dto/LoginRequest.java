0e4dcb3f15487ee49bdfc481e5d6192feae432d5
michaltu <michal.tusz12@gmail.com>
2024-03-09 21:49:53 +0100

    modified login dto to store identifier

--- src/main/java/com/tradingApp/tradingApp/dto/LoginRequest.java
+++ src/main/java/com/tradingApp/tradingApp/dto/LoginRequest.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
-    private String email;
+    private String identifier;
     private String password;
 }
