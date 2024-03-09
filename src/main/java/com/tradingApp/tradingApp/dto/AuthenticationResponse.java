4cd2ba983b153dcefb69f7e13fbfd745b6e51180
michaltu <michal.tusz12@gmail.com>
2024-03-09 21:40:57 +0100

    Added auth response dto

---
+++ src/main/java/com/tradingApp/tradingApp/dto/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.tradingApp.tradingApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String authenticationToken;
+    private String refreshToken;
+    private Instant expiresAt;
+    private String username;
+}
