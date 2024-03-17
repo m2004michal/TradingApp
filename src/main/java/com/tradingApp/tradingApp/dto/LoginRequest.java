cec1c1e8ab64cbd3a0c6eecd39bee7871ffc033e
michaltu <michal.tusz12@gmail.com>
2024-03-17 20:46:54 +0100

    added keys and started working on jwtProvider

--- src/main/java/com/tradingApp/tradingApp/dto/LoginRequest.java
+++ src/main/java/com/tradingApp/tradingApp/dto/LoginRequest.java
@@ -2,11 +2,13 @@ package com.tradingApp.tradingApp.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class LoginRequest {
     private String identifier;
     private String password;
