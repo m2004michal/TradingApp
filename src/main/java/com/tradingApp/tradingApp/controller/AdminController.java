88934ff341942fb382bd745665d7425fb2306de4
micha <michal.tusz12@gmail.com>
2024-04-21 19:51:57 +0200

    .

--- src/main/java/com/tradingApp/tradingApp/controller/AdminController.java
+++ src/main/java/com/tradingApp/tradingApp/controller/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.stream.Collectors;
 
 @RequestMapping("api/admin")
 @Controller
