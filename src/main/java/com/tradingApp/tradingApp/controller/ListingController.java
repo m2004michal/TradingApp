8d671c3ef8717c20e64c4b8fe9c4d35658cf2fc5
micha <michal.tusz12@gmail.com>
2024-05-22 21:36:34 +0200

    preparation for creation of listing api

---
+++ src/main/java/com/tradingApp/tradingApp/controller/ListingController.java
@@ -0,0 +1,11 @@
+package com.tradingApp.tradingApp.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/listings")
+@Controller
+@AllArgsConstructor
+public class ListingController {
+}
---
+++ src/main/java/com/tradingApp/tradingApp/repository/ListingRepository.java
@@ -0,0 +1,13 @@
+package com.tradingApp.tradingApp.repository;
+
+import com.tradingApp.tradingApp.model.Listing;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ListingRepository extends PagingAndSortingRepository<Listing, Long> {
+    Optional<List<Listing>> findAllByPromoted(boolean isPromoted);
+}
