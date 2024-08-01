03c6b22cb0117ea3a7ea48f47e4b3a4724df9047
micha <michal.tusz12@gmail.com>
2024-08-01 13:22:33 +0200

    forgot to write about listing mapper in the last commit :/ Also started creating listing service

---
+++ src/main/java/com/tradingApp/tradingApp/service/ListingService.java
@@ -0,0 +1,16 @@
+package com.tradingApp.tradingApp.service;
+
+import com.tradingApp.tradingApp.dto.ListingRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ListingService {
+
+    public void save(ListingRequest listingRequest) {
+
+
+    }
+}
