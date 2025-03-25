ba7992b2f5f03b38e46577782717e3c03907f6fa
micha <michal.tusz12@gmail.com>
2025-03-25 14:43:15 +0100

    implemented deleteListing api

--- src/main/java/com/tradingApp/tradingApp/controller/ListingController.java
+++ src/main/java/com/tradingApp/tradingApp/controller/ListingController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RequestMapping("api/listings")
@@ -19,8 +20,36 @@ public class ListingController {
     private final ListingService listingService;
 
     @PostMapping("/create")
-    public ResponseEntity<String> createListing(@ModelAttribute ListingRequestWithNoPhotos listingRequestWithNoPhotos, @RequestParam("images") List<MultipartFile> images) {
-        listingService.save(listingRequestWithNoPhotos, images);
-        return new ResponseEntity<>("Post created succesfully", HttpStatus.CREATED);
+    public ResponseEntity<String> createListing(@RequestParam("title") String title,
+                                                @RequestParam("description") String description,
+                                                @RequestParam("quantity") int quantity,
+                                                @RequestParam("price") BigDecimal price,
+                                                @RequestParam("isForTrade") boolean isForTrade,
+                                                @RequestParam("isForSale") boolean isForSale,
+                                                @RequestParam("isNegotiable") boolean isNegotiable,
+                                                @RequestParam("isQuickBuy") boolean isQuickBuy,
+                                                @RequestParam("gameName") String gameName,
+                                                @RequestParam("categoryName") String categoryName,
+                                                @RequestParam("images") List<MultipartFile> images) {
+        listingService.save(ListingRequestWithNoPhotos.builder()
+                .title(title)
+                .description(description)
+                .quantity(quantity)
+                .price(price)
+                .isForTrade(isForTrade)
+                .isForSale(isForSale)
+                .isNegotiable(isNegotiable)
+                .isQuickBuy(isQuickBuy)
+                .gameName(gameName)
+                .categoryName(categoryName)
+                .build(), images);
+        return new ResponseEntity<>("Post created successfully", HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/delete/{listingId}")
+    public ResponseEntity<String> deleteListing(@PathVariable Long listingId) {
+        return new ResponseEntity<>(listingService.deleteListing(listingId), HttpStatus.CREATED);
+
+    }
+
 }
--- src/main/java/com/tradingApp/tradingApp/service/ListingService.java
+++ src/main/java/com/tradingApp/tradingApp/service/ListingService.java
@@ -3,12 +3,19 @@ package com.tradingApp.tradingApp.service;
 import com.tradingApp.tradingApp.dto.ListingRequestWithNoPhotos;
 import com.tradingApp.tradingApp.mapper.ListingMapper;
 import com.tradingApp.tradingApp.model.Listing;
+import com.tradingApp.tradingApp.model.UserEntity;
 import com.tradingApp.tradingApp.repository.ListingRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @AllArgsConstructor
@@ -26,6 +33,29 @@ public class ListingService {
 
     public void savePhotosForListing(Long listingId, List<MultipartFile> multipartFiles){
         multipartFiles.forEach((photo) -> imageDataService.uploadListingPicture(listingId, photo));
+    }
+
+    public String deleteListing(Long listingId) {
+        AtomicReference<String> toReturn = new AtomicReference<>("Failed to delete listing");
+        if (listingRepository.existsById(listingId)) {
+            listingRepository.findById(listingId).ifPresent(listing -> {
+                if (isLoggedInUserAdmin() || isLoggedInUserOwnerOfListing(listing)) {
+                    listingRepository.delete(listing);
+                    toReturn.set("Listing deleted successfully");
+                }
+            });
+            return toReturn.get();
+        }
+        return toReturn.get();
+    }
+
+    public boolean isLoggedInUserAdmin(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().toString().contains("ADMIN");
+    }
+    public boolean isLoggedInUserOwnerOfListing(Listing listing){
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return jwt.getClaim("sub").equals(listing.getUserEntity().getUsername());
     }
 
 
