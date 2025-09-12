980dfe43c7170618c6496c0f4d155fe926dfb006
micha <michal.tusz12@gmail.com>
2025-09-12 19:12:14 +0200

    wh test

--- src/main/java/com/tradingApp/tradingApp/TradingAppApplication.java
+++ src/main/java/com/tradingApp/tradingApp/TradingAppApplication.java
@@ -13,6 +13,7 @@ public class TradingAppApplication {
 
 
 	public static void main(String[] args){
+		System.out.println("Hello World");
 		SpringApplication.run(TradingAppApplication.class, args);
 	}
 }
